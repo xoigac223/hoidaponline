d7ccc2b8f34db1010fc2ab366a1b0f1ae9cf2bcf
xoigac223 <54257166+xoigac223@users.noreply.github.com>
2020-10-17 11:56:44 +0700

    12h

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS, cross_origin
+from user import User, users
 import requests
  
 app = Flask(__name__)
-
-@app.route('/')
+uname = None
+@app.route('/', methods=["GET"])
 def index():
-	return render_template('index.html')
+	return render_template('index.html', Template_uname = uname)
 
 @app.route('/c')
 def hoc_c():
@@ -21,12 +22,24 @@ def login():
 	if len(request.form) > 0:
 		username = request.form['username']
 		password = request.form['password']
-		list_users = requests.get("http://127.0.0.1:5000/user/{}/{}".format(username, password)).json()
-		if len(list_users) == 1:
-			requests.put("http://127.0.0.1:5000/update/{}".format(list_users[0]['id']))
-			return redirect(url_for("index"))
+		user = requests.get("http://127.0.0.1:5000/user/{}/{}".format(username, password)).json()
+		if len(user) == 1:
+			id = user[0]['id']
+			uname = user[0]['username']
+			pword = user[0]['password']
+			status = user[0]['status']
+			coin = user[0]['coin']
+			u = User(id, uname, pword, status, coin)
+			users[uname] = u
+			requests.put("http://127.0.0.1:5000/update/{}".format(u.id))
+			return redirect(url_for('nguoidung', uname = uname))
 	return render_template('login.html')
 	
-
-	
+@app.route('/<uname>',methods=["GET", "POST"])
+def nguoidung(uname):
+	user ={}
+	user[uname] = users[uname].uname
+	user['status'] = users[uname].status
+	user['coin'] = users[uname].coin
+	return render_template('user.html', Template_uname = uname, user = user)
 CORS(app)	
---
+++ user.py
@@ -0,0 +1,11 @@
+class User():
+    def __init__(self, id, uname, pword, status, coin):
+        self.id = id
+        self.uname = uname
+        self.pword = pword
+        self.status = status
+        self.coin = coin
+    
+
+users = {}
+    

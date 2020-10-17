6064d39d9eaefd77468770ae30ee0ed356e7cf8d
xoigac223 <54257166+xoigac223@users.noreply.github.com>
2020-10-17 08:16:24 +0700

    8:16

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS, cross_origin
+import requests
  
 app = Flask(__name__)
 
@@ -7,4 +8,25 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/c')
+def hoc_c():
+	return render_template('c.html')
+
+@app.route('/roadmap/frontend')
+def roadmap_frontend():
+	return render_template('frontend.html')
+
+@app.route('/login', methods=["GET", "POST", "PUT"])
+def login():
+	if len(request.form) > 0:
+		username = request.form['username']
+		password = request.form['password']
+		list_users = requests.get("http://127.0.0.1:5000/user/{}/{}".format(username, password)).json()
+		if len(list_users) == 1:
+			requests.put("http://127.0.0.1:5000/update/{}".format(list_users[0]['id']))
+			return redirect(url_for("index"))
+	return render_template('login.html')
+	
+
+	
 CORS(app)	
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from db_config import mysql
 from flask import jsonify
 from flask import flash, request
 
-@app.route('/user')
-def student():
+@app.route('/user/<username>/<password>')
+def user(username, password):
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		cursor.execute("SELECT * FROM users")
-		rows = cursor.fetchall()
-		res = jsonify(rows)
+		cursor.execute("SELECT * FROM users WHERE username='{}' and password='{}'".format(username, password))
+		row = cursor.fetchall()
+		res = jsonify(row)
 		res.status_code = 200
 		return res
 	except Exception as e:
@@ -20,5 +20,38 @@ def student():
 		cursor.close() 
 		conn.close()
 
+@app.route('/users')
+def users():
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+		cursor.execute("SELECT * FROM users")
+		rows = cursor.fetchall()
+		res = jsonify(rows)
+		res.status_code = 200
+ 
+		return res
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
+
+@app.route('/update/<int:id>', methods=['PUT'])
+def update_user(id):
+	try:
+		sql = "UPDATE users SET status = 1 WHERE id={}".format(id)
+		conn = mysql.connect()
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		conn.commit()
+		res = jsonify('User updated successfully.')
+		res.status_code = 200
+		return res
+	except Exception as e:
+		print(e)
+	finally:
+		cursor.close() 
+		conn.close()
 if __name__ == "__main__":
 	app.run()

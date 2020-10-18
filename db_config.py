d65552ba66d892113ccbf7d6e9a56f827c8b79cf
xoigac223 <54257166+xoigac223@users.noreply.github.com>
2020-10-18 09:51:16 +0700

    'fix db_config, add requirement'

--- db_config.py
+++ db_config.py
@@ -3,9 +3,9 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'XoiGac223'
-app.config['MYSQL_DATABASE_DB'] = 'mindx_demo'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'sql12371461'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'fiDgrddexu'
+app.config['MYSQL_DATABASE_DB'] = 'sql12371461'
+app.config['MYSQL_DATABASE_HOST'] = 'sql12.freemysqlhosting.net'
 
 mysql.init_app(app)
